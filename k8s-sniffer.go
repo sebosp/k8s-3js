54d0c0511d679fc3c88d8f4a38780707dea12153
Seb Ospina <kraige@gmail.com>
2018-11-07 19:13:21 +0100

    Adding initial Pod List

--- k8s-sniffer.go
+++ k8s-sniffer.go
@@ -2,12 +2,26 @@ package main
 
 // Adapted from https://www.youtube.com/watch?v=qiB4RxCDC8o
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
+
+func printPods(pods *v1.PodList) {
+	template := "%-32s%-8s\n"
+	fmt.Printf(template, "NAME", "STATUS")
+	for _, pod := range pods.Items {
+		fmt.Printf(
+			template,
+			pod.Name,
+			string(pod.Status.Phase))
+	}
+}
 
 func main() {
 	cfg, err := rest.InClusterConfig()
@@ -15,9 +29,16 @@ func main() {
 		log.Printf("Error configuring kube client: %s", err)
 		os.Exit(1)
 	}
-	cl, err := kubernetes.NewForConfig(cfg)
+	clientSet, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
 		log.Printf("Error creatinga new client: %s", err)
 		os.Exit(1)
 	}
+	api := clientSet.CoreV1()
+	listOptions := metav1.ListOptions{}
+	pods, err := api.Pods("default").List(listOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	printPods(pods)
 }
